7b444debafb56b4999893d650ccf4c16e3bc4d26
madolphe <madolphe@ensc.fr>
2019-09-19 11:46:43 -0400

    Merge branch 'master' of /Users/adolphemaxime/PycharmProjects/TechniqueApprentissage1-RegressionLineaire/RegressionLineaire with conflicts.

--- solution_regression.py
+++ solution_regression.py
@@ -2,7 +2,7 @@
 
 #####
 # JOUFFROY Emma
-# ADOLPHE Maxime - 79 156 789 @TODO verif le matricule et ajouter celui de Emm
+# ADOLPHE Maxime - 19 156 789 @TODO verif le matricule et ajouter celui de Emm
 ###
 import gestion_donnees as gd
 import numpy as np
@@ -23,15 +23,22 @@ class Regression:
         Si x est un scalaire, alors phi_x sera un vecteur à self.M dimensions : (x^0,x^1,x^2,...,x^self.M-1)
         Si x est un vecteur de N scalaires, alors phi_x sera un tableau 2D de taille NxM
 
+        x: données d'apprentissage à envoyer dans un "nouvel" espace selon une base polynomiale
+        using_sklearn: booléen indiquant l'utilisation de la bibliothèque scikit-learn ou non
+
         NOTE : En mettant phi_x = x, on a une fonction de base lineaire qui fonctionne pour une regression lineaire
         """
         if not using_sklearn:
-            # Après avoir testé les résultats renvoyés par scikit learn, la fonction de base polynomiale renvoie
-            # un tableau 2D de float64. C'est pour cel que nous avons décidé de caster nos données
+            # Création du vecteur de taille NxM
             phi_x = np.array([x**i for i in range(0, self.M)]).T.astype(float)
+            # Note: Le tableau est caster en float même si x est un vecteur d'entier afin de coller avec le
+            # fonctionnement de scikit_learn
         else:
+            # Création d'un objet "PolynomialFeatures" de degrès M-1 (afin d'avoir M composantes)
             poly = PolynomialFeatures(degree=self.M - 1)
+            # Reshape afin que x soit toujours un tableau contenant N lignes et 1 colonne:
             poly_x = np.array(x).reshape(-1, 1)
+            # Methode de l'instance poly pour transformer x en phi_x
             phi_x = poly.fit_transform(poly_x)
         return phi_x
 
@@ -48,28 +55,52 @@ class Regression:
         Mmax : plus grande valeur de M à tester
 
         """
+        # On commence par redimensionner t (vecteur cible) comme un vecteur à N lignes et 1 colonne afin de le
+        # concatener par la suite
         t = np.expand_dims(t, axis=1)
+        # On instancie des listes qui comporteront les m et l'erreur d'apprentissage correspondante
+        # Rq: Ces listes seront utilisées en même temps afin de faire correspondre les indices des erreurs et des M
+        # correspondant
         liste_m = []
         liste_erreurs = []
+        # On calcule le nombre de valeur de notre jeu de validation (ici on choisit de considérer 20% du jeu
+        # d'apprentissage)
         taille_validation = int(np.round(X.shape[0]/5))
         for M in range(Mmin, Mmax+1):
             error_val_total = 0
+            # la methode fonction_base_polynomiale utilise le champ M on le modifie donc directement:
             self.M = M
             phi_X = self.fonction_base_polynomiale(X)
+            # Afin de "découper" on concatene phi_X et t (on a donc un np.array de taille (NxM+1))
             D = np.concatenate((phi_X, t), axis=1)
             for j in range(0, 10):
+                # 10-fold cross-validation => considérer 10 couples jeu d'apprentissage / jeu de validation
                 np.random.shuffle(D)
+                # X_train prend les exemples de taille_validation jusqu'à la fin en retirant la colonne cible
+                # t_train prend les exemples de taille_validation jusqu'à la fin en ne considérant que la colonne cible
                 (X_train, t_train) = (D[taille_validation:, :-1], D[taille_validation:, D.shape[1]-1])
+                # X_val prend les exemples jusqu'à taille_validation en retirant la colonne cible
+                # t_train prend les exemples jusqu'à taille_validation en ne considérant que la colonne cible
                 (X_val, t_val) = (D[0:taille_validation, :-1], D[0:taille_validation, D.shape[1]-1])
+                # On cherche les paramètres de notre modèle sur le jeu d'apprentissage x_train:
                 Wmap = np.linalg.solve((X_train.T.dot(X_train) + np.identity(X_train.shape[1])*self.lamb),
                                        (X_train.T.dot(t_train)))
+                # On cherche ensuite l'erreur sur le jeu d'apprentissage:
                 t_pred_val = X_val.dot(Wmap)
                 error_val_total += self.erreur(t_val, t_pred_val)
+            # Pour un M donné on calcule finalement l'erreur moyenne sur le jeu de validation
             erreur_val_moy = error_val_total / 10
             liste_m.append(M)
             liste_erreurs.append(erreur_val_moy)
         index_best_m = liste_erreurs.index(min(liste_erreurs))
         self.M = liste_m[index_best_m]
+        # On effectue une petite visualisation des erreurs de validation en fonction de M:
+        plt.figure()
+        plt.plot(liste_m, liste_erreurs, marker='*')
+        plt.xlabel("M testé")
+        plt.ylabel("Erreur de validation moyenne")
+        plt.title("Résultats de la 10-cross validation \n M choisi: {}".format(self.M))
+        plt.show()
         print("Meilleure valeur de l'hyperparamètre M trouvé lors de la 10-fold cross-validation : {}".format(self.M))
 
     def entrainement(self, X, t, using_sklearn=False):
@@ -95,11 +126,13 @@ class Regression:
         Aussi, la variable membre self.M sert à projeter les variables X vers un espace polynomiale de degre M
         (voir fonction self.fonction_base_polynomiale())
 
-        NOTE IMPORTANTE : lorsque self.M <= 0, il faut trouver la bonne valeur de self.M
+        X: jeu de donnée d'exemples
+        t: jeu de données cibles
 
+        NOTE IMPORTANTE : lorsque self.M <= 0, il faut trouver la bonne valeur de self.M
         """
         if self.M <= 0:
-            self.recherche_hyperparametre(X, t, Mmin=1, Mmax=10)
+            self.recherche_hyperparametre(X, t, Mmin=1, Mmax=30)
 
         phi_x = self.fonction_base_polynomiale(X, using_sklearn=using_sklearn)
 
@@ -111,7 +144,7 @@ class Regression:
             reg.fit(phi_x, t)
             self.w = reg.coef_
             print('w trouvé : {}'.format(self.w))
-        return phi_x
+        return
 
     def prediction(self, x, using_sklearn):
         """
@@ -121,6 +154,8 @@ class Regression:
         Cette methode suppose que la methode ``entrainement()``
         a prealablement ete appelee. Elle doit utiliser le champs ``self.w``
         afin de calculer la prediction y(x,w) (equation 3.1 et 3.3).
+        x: donnée dont on veut prédire la cible
+        using_sklearn: booléen indiquant la méthode de prédiction
         """
         phi_x = self.fonction_base_polynomiale(x, using_sklearn=using_sklearn)
         prediction = phi_x.dot(self.w)
@@ -132,14 +167,18 @@ class Regression:
         Retourne l'erreur de la difference au carre entre
         la cible ``t`` et la prediction ``prediction``.
         On ne considère pas la régression avec prédictions multiples ( donc prediction et p sont des vecteurs à une dimension )
+        t: cible
+        prediction: prediction effectuée par un modèle
         """
-
         mse = np.sum((prediction-t)**2)
         return mse
 
 
 if __name__ == '__main__':
-
+    """
+    Liste de fonctions mises en place pour tester certaines partie de la classe présentée précédemment. 
+    # Non utile pour le bon fonctionnement du projet. 
+    """
     def test_fonction_base_polynomiale():
         reg = Regression(lamb=5, m=5)
         x_int = 2
